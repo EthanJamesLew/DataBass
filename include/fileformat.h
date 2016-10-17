4bf29734ea3c2cc70aecc534f81a800cbc1c14c8
Ethan Lew <ethan.lew@pcc.edu>
2016-10-17 10:23:24 -0700

    Update fileformat.h

--- include/fileformat.h
+++ include/fileformat.h
@@ -1,13 +1,44 @@
-#ifndef FILEFORMAT_H
-#define FILEFORMAT_H
+#include "fileformat.h"
 
-#include "estring.h"
-#include "songlibrary.h"
+void FileFormat::projectToFile(SongLibrary& lib, e::EString filename)
+{
+	std::ofstream file;
+	file.open(filename.cstr());
+	for (unsigned i = 0; i < lib.getSize(); i++)
+	{
+		Song* e = lib.searchOrder(i);
+		file << (*e)[TITLE] << ";";
+		file << (*e)[ARTIST] << ";";
+		file << (*e)[MINUTES] << ";";
+		file << (*e)[SECONDS] << ";";
+		file << (*e)[ALBUM] << std::endl;
+	}
+	file.close();
+}
 
-#include <fstream>
-#include <cstring>
+void FileFormat::fileToProject(SongLibrary& lib, e::EString filename)
+{
+	std::ifstream file;
+	file.open(filename.cstr());
+	while (file.peek() != EOF)
+	{
+		char line[256];
+		file.getline(line, 256);
+		e::EString li = (e::EString) line;
+		e::EVector<e::EString> songItems = getWords(li, ';');
+		if (songItems.size() != 5) break;//Throw Error
+		Song e;
+		e[TITLE] = songItems[TITLE];
+		e[ARTIST] = songItems[ARTIST];
+		e[MINUTES] = songItems[MINUTES];
+		e[SECONDS] = songItems[SECONDS];
+		e[ALBUM] = songItems[ALBUM];
+		lib.addItem(e);
+	}
+	file.close();
+}
 
-e::EVector<e::EString> getWords(e::EString& vec, const char del = ' ')
+e::EVector<e::EString> getWords(e::EString& vec, const char del)
 {
 	e::EVector<e::EString> words;
 	int prev = 0;
@@ -24,48 +55,3 @@ e::EVector<e::EString> getWords(e::EString& vec, const char del = ' ')
 	words.push_back(vec.slice(prev, vec.size()));
 	return words;
 }
-
-class FileFormat
-{
-public:
-	void projectToFile(SongLibrary& lib, e::EString filename)
-	{
-		std::ofstream file;
-		file.open(filename.cstr());
-		for (unsigned i = 0; i < lib.getSize(); i++)
-		{
-			Song* e = lib.searchOrder(i);
-			file << (*e)[TITLE] << ";";
-			file << (*e)[ARTIST] << ";";
-			file << (*e)[MINUTES] << ";";
-			file << (*e)[SECONDS] << ";";
-			file << (*e)[ALBUM] << std::endl;
-		}
-		file.close();
-	}
-
-	void fileToProject(SongLibrary& lib, e::EString filename)
-	{
-		std::ifstream file;
-		file.open(filename.cstr());
-		while (file.peek() != EOF)
-		{
-			char line[256];
-			file.getline(line, 256);
-			e::EString li = (e::EString) line;
-			e::EVector<e::EString> songItems = getWords(li, ';');
-			if (songItems.size() != 5) break;//Throw Error
-			Song e;
-			e[TITLE] = songItems[TITLE];
-			e[ARTIST] = songItems[ARTIST];
-			e[MINUTES] = songItems[MINUTES];
-			e[SECONDS] = songItems[SECONDS];
-			e::EString album = songItems[ALBUM]; 
-			e[ALBUM] = album.slice(0,album.size()-1 );
-			
-			lib.addItem(e);
-		}
-		file.close();
-	}
-};
-#endif
